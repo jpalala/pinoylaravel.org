406cd42654c8db27dd801caf5efe72cf4e49d9e3
Joe Palala <jose.palala@live.com>
2019-02-18 00:54:20 +0000

    adding techmeetupsph key and created jobs queue for the email newsletter and notifications

---
+++ resources/views/templates/join_writers.blade.php
@@ -0,0 +1,2 @@
+on ulamko goa polars text buy bitcoin at <a href="https://www.bitbit.cash/">bitbit</a> or <a href="https://coins.ph">coins.ph</a>. no worries put a litus mone watch it grow. If you're a great writer please email <a href="mailto:jose.palala@gmail.com">joe</a> or contact him on the facebook group to replace this text. </p>
+      
